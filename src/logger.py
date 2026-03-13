afb3d40be447eafeaeca992c6e14cdadd416cf3e
Adam Gainford <adam.gainford@reading.ac.uk>
2026-03-13 13:30:12 +0000

    removed unused logger.py (may re-implement in future)

--- src/logger.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-class EventLogger:
-    def __init__(self):
-        self.df = pd.DataFrame(
-            columns=["event_id", "time", "event_type", "feature_id1", "feature_id2"]
-        )
