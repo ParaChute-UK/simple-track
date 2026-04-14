759ae3fb585b280a13cc36d80c3fdf09d8809cec
Adam Gainford <adam.gainford@reading.ac.uk>
2026-04-14 17:35:15 +0100

    fixed bug with entry point help tool

--- src/run_simple_track.py
+++ src/run_simple_track.py
@@ -1,23 +1,20 @@
 import argparse
-import sys
 
 from simpletrack import Tracker
 
 
 def run_tracking():
-    if len(sys.argv) < 2:
-        raise Exception("Running SimpleTrack requires path to at least one config")
+    # TODO: make argparser default way of handling inputs, including configs and loaders
+    # Need to make sure that changes don't affect pyproject.toml entry points
+    # easiest just to pass the parser in to run_tracking.
+    parser = argparse.ArgumentParser(description="Run Simple-Track")
+    parser.add_argument("configs", help="Path to one or more yaml config files.")
+    args = parser.parse_args()
 
-    config_paths = sys.argv[1:]
-    for config_path in config_paths:
+    for config_path in args.configs:
         # With None passed into run method, uses input path in config
         Tracker(config_path).run()
 
 
 if __name__ == "__main__":
-    # TODO: make argparser default way of handling inputs, including configs and loaders
-    # Need to make sure that changes don't affect pyproject.toml entry points
-    # easiest just to pass the parser in to run_tracking.
-    msg = "Run Simple-Track. Requires path to at least one yaml config file"
-    parser = argparse.ArgumentParser(description=msg)
     run_tracking()
