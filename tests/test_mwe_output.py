178019debd85f69ca5dc2cb19e7363eda92466e5
Adam Gainford <adam.gainford@reading.ac.uk>
2026-05-27 18:08:12 +0100

    populated mwe tests

--- tests/test_mwe_output.py
+++ tests/test_mwe_output.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import numpy as np
 import pytest
 
+from simpletrack.feature import Feature
+from simpletrack.frame import Timeline
 from simpletrack.track import Tracker
 
 
@@ -70,7 +72,7 @@ def generate_mwe_files(save_path=None):
 # pytest fixture with scope "session" means this setup will only run once, with the output used by
 # any test that includes "mwe_timeline" as arg input
 @pytest.fixture(scope="session")
-def mwe_timeline():
+def mwe_timeline() -> Timeline:
     mwe_fields = generate_mwe_files()
 
     # Construct dict for passing to SimpleTrack
@@ -81,7 +83,6 @@ def mwe_timeline():
     }
 
     mwe_config = {
-        "DATETIME": {"start_time": "2012-08-25 14:05:00", "time_interval": 5},
         "FEATURE": {
             "threshold": 0.5,
             "under_threshold": False,
@@ -97,19 +98,236 @@ def mwe_timeline():
 
 
 def test_first_mwe_outputs(mwe_timeline):
-    # TODO
+    """
+    Test that a single feature exists in the first frame
+    with no parent, children, and lifetime of 1, with expected
+    size and centroid.
+
+    Also test that there is no flow in the first frame,
+    and that the feature is correctly identified as a new feature
+    """
     base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
     frame = mwe_timeline.get_frame(base_time)
-    print(frame)
+
+    # test there is a single feature
+    assert len(frame.features) == 1
+
+    # test feature properties
+    feature = frame.get_feature(1)
+    assert isinstance(feature, Feature)
+    assert feature.id == 1
+    assert feature.lifetime == 1
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (19.5, 19.5)
+    assert feature.get_size() == 400
+
+    # Test there is no flow
+    assert feature.dydx == ()
+    assert frame.get_flow() == (None, None)
 
 
 def test_second_mwe_outputs(mwe_timeline):
-    # TODO
+    """
+    Test that there is still a single feature with the same id
+    as the feature in the first frame and with an incremented lifetime
+    Also test that there is a flow from the first frame to the second
+    (although don't need to test exact flow values, just that they are nonzero)
+
+    Also test that there is still no parent or children, and the centroid
+    has been updated as expected
+
+    """
     base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
     mwe_idx = 1
     frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
     frame = mwe_timeline.get_frame(frame_time)
-    print(frame)
+
+    # test there is a single feature
+    assert len(frame.features) == 1
+
+    # test feature properties
+    feature = frame.get_feature(1)
+    assert isinstance(feature, Feature)
+    assert feature.id == 1
+    assert feature.lifetime == 2
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (24.5, 19.5)
+    assert feature.get_size() == 400
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
+
+
+def test_third_mwe_outputs(mwe_timeline):
+    """
+    Test that there are now two features, with the first having the same
+    id as the feature in the second frame and an incremented lifetime, and
+    the second feature having a new id and a lifetime of 1. Also test that
+    this new feature is not a child of the first feature, and this it is
+    correctly identified as a new feature
+    """
+    base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
+    mwe_idx = 2
+    frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
+    frame = mwe_timeline.get_frame(frame_time)
+
+    # test there are two features
+    assert len(frame.features) == 2
+
+    # test feature properties for feature 1
+    feature = frame.get_feature(1)
+    assert isinstance(feature, Feature)
+    assert feature.id == 1
+    assert feature.lifetime == 3
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (29.5, 19.5)
+    assert feature.get_size() == 400
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(2)
+    assert isinstance(feature, Feature)
+    assert feature.id == 2
+    assert feature.lifetime == 1
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (24.5, 59.5)
+    assert feature.get_size() == 400
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
+
+
+def test_fourth_mwe_outputs(mwe_timeline):
+    """
+    Test that the first feature is no longer present, and that the second
+    feature has the same id as in the previous frame, and with an incremented
+    lifetime.
+
+    """
+    base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
+    mwe_idx = 3
+    frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
+    frame = mwe_timeline.get_frame(frame_time)
+
+    # test there we are back to one feature
+    assert len(frame.features) == 1
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(2)
+    assert isinstance(feature, Feature)
+    assert feature.id == 2
+    assert feature.lifetime == 2
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (29.5, 59.5)
+    assert feature.get_size() == 400
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
+
+
+def test_fifth_mwe_outputs(mwe_timeline):
+    """
+    Test that the second feature advects as expected
+    """
+    base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
+    mwe_idx = 4
+    frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
+    frame = mwe_timeline.get_frame(frame_time)
+
+    # test there we are back to one feature
+    assert len(frame.features) == 1
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(2)
+    assert isinstance(feature, Feature)
+    assert feature.id == 2
+    assert feature.lifetime == 3
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.centroid == (34.5, 59.5)
+    assert feature.get_size() == 400
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
+
+
+def test_sixth_mwe_outputs(mwe_timeline):
+    """
+    Test that the feature has split into two, with one feature retaining the
+    previous id and an incremented lifetime, and the other feature having a new id
+    and a retained lifetime of 4. Also test that the new feature is correctly identified as
+    a child, and the old feature is a parent.
+    """
+    base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
+    mwe_idx = 5
+    frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
+    frame = mwe_timeline.get_frame(frame_time)
+
+    # test there we are back to one feature
+    assert len(frame.features) == 2
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(2)
+    assert isinstance(feature, Feature)
+    assert feature.id == 2
+    assert feature.lifetime == 4
+    assert feature.parent is None
+    assert feature.children == [3]
+    assert feature.centroid == (39.5, 52.5)
+    assert feature.get_size() == 200
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(3)
+    assert isinstance(feature, Feature)
+    assert feature.id == 3
+    assert feature.lifetime == 4  # retain_lifetime_on_split defaults to True
+    assert feature.parent == 2
+    assert feature.children is None
+    assert feature.centroid == (39.5, 66.5)
+    assert feature.get_size() == 200
+
+
+def test_seventh_mwe_outputs(mwe_timeline):
+    """
+    Test that the two features have merged back into one, with the same id
+    being retained from the older timestep and with an incremented lifetime.
+    Also test that the merged feature has correctly identified the split feature
+    as being accreted by the resulting feature.
+    """
+    base_time = dt.datetime(2024, 1, 1, 0, 0, 0)
+    mwe_idx = 6
+    frame_time = base_time + dt.timedelta(minutes=5 * int(mwe_idx))
+    frame = mwe_timeline.get_frame(frame_time)
+
+    # test there we are back to one feature
+    assert len(frame.features) == 1
+
+    # test feature properties for feature 2
+    feature = frame.get_feature(2)
+    assert isinstance(feature, Feature)
+    assert feature.id == 2
+    assert feature.lifetime == 5
+    assert feature.parent is None
+    assert feature.children is None
+    assert feature.accreted == [3]
+    assert feature.centroid == (42, 59.5)
+    assert feature.get_size() == 500
+
+    # Test there is a flow across the feature
+    assert feature.dydx != ()
+    assert np.all(frame.get_flow()) is not None
 
 
 if __name__ == "__main__":
